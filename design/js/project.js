b05c0e99e8d98b308b1ee0bb9aa3af70d64ca096
Josh Green <greenjm@rose-hulman.edu>
2015-10-20 16:44:12 -0400

    Added design for project Editor page

---
+++ design/js/project.js
@@ -0,0 +1,39 @@
+$(document).ready(function() {
+
+	var addFilesHandler = function(isVisible) {
+		$(".files").unbind("click");
+		if (isVisible) {
+			$(".files").on("click", function() {
+				$(".files").css("right", "0px");
+				$(".file-list").hide();
+				addFilesHandler(false);
+			});
+		} else {
+			$(".files").on("click", function() {
+				$(".files").css("right", "200px");
+				$(".file-list").show();
+				addFilesHandler(true);
+			});
+		}
+	}
+
+	var addConsoleHandler = function(isVisible) {
+		$(".console-toggle").unbind("click");
+		if (isVisible) {
+			$(".console-toggle").on("click", function() {
+				$(".console-toggle").css("bottom", "0px");
+				$(".console").hide();
+				addConsoleHandler(false);
+			});
+		} else {
+			$(".console-toggle").on("click", function() {
+				$(".console-toggle").css("bottom", "200px");
+				$(".console").show();
+				addConsoleHandler(true);
+			});
+		}
+	}
+
+	addFilesHandler(false);
+	addConsoleHandler(false);
+});
