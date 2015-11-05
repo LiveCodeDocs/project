fdc1662daf269d701d22604c16fe236d914c5f0a
bonattt <bonattt@rose-hulman.edu>
2015-11-05 17:29:17 -0500

    made a change to unit test to try to make it run in py2.7

--- design/codeChangesUnitTest.py
+++ design/codeChangesUnitTest.py
@@ -3,13 +3,13 @@
 '''
 
 import unittest
-import codeChanges as c
+import codeChanges
 
 def enqueueSomeChanges(code):
        
-    change1 = c.Change(0, c.Change.insert, 13, " -- this line has been changed")
-    change2 = c.Change(1, c.Change.insert, 13, " -- this line has been changed")
-    change3 = c.Change(2, c.Change.insert, 13, " -- this line has been changed")
+    change1 = codeChanges.Change(0, codeChanges.Change.insert, 13, " -- this line has been changed")
+    change2 = codeChanges.Change(1, codeChanges.Change.insert, 13, " -- this line has been changed")
+    change3 = codeChanges.Change(2, codeChanges.Change.insert, 13, " -- this line has been changed")
         
     code.enqueueChange(change1)
     code.enqueueChange(change2)
@@ -18,7 +18,7 @@ def enqueueSomeChanges(code):
 class TestCodeChanges(unittest.TestCase):
     
     def setUp(self):
-        self.code = c.Code("", 0)
+        self.code = codeChanges.Code("", 0)
         self.code.linesOfCode = ["this is line 1",
                             "this is line 2",
                             "this is line 3",
@@ -39,16 +39,16 @@ class TestCodeChanges(unittest.TestCase):
         
     def testDequeueChangeReturnsFirstAdded(self):
         
-        change1 = c.Change(3, c.Change.insert, 13, " -- this line has been changed")
+        change1 = codeChanges.Change(3, codeChanges.Change.insert, 13, " -- this line has been changed")
         self.code.enqueueChange(change1)
         enqueueSomeChanges(self.code)
         self.assertTrue(self.code.dequeueChange() is change1)
         
     def testRemoveLineShiftsLinesBelow(self):
-        change1 = c.Change(0, c.Change.insert, 13, " -- this line has been changed")
-        change2 = c.Change(1, c.Change.insert, 13, " -- this line has been changed")
-        change3 = c.Change(5, c.Change.insert, 13, " -- this line has been changed")
-        change4 = c.Change(6, c.Change.insert, 13, " -- this line has been changed")
+        change1 = codeChanges.Change(0, codeChanges.Change.insert, 13, " -- this line has been changed")
+        change2 = codeChanges.Change(1, codeChanges.Change.insert, 13, " -- this line has been changed")
+        change3 = codeChanges.Change(5, codeChanges.Change.insert, 13, " -- this line has been changed")
+        change4 = codeChanges.Change(6, codeChanges.Change.insert, 13, " -- this line has been changed")
         self.code.enqueueChange(change1)
         self.code.enqueueChange(change2)
         self.code.enqueueChange(change3)
@@ -62,10 +62,10 @@ class TestCodeChanges(unittest.TestCase):
         self.assertEqual(original4 -1, change4.lineNumber)
     
     def testRemoveLineDoesNotShiftLinesAbove(self):
-        change1 = c.Change(0, c.Change.insert, 13, " -- this line has been changed")
-        change2 = c.Change(1, c.Change.insert, 13, " -- this line has been changed")
-        change3 = c.Change(5, c.Change.insert, 13, " -- this line has been changed")
-        change4 = c.Change(6, c.Change.insert, 13, " -- this line has been changed")
+        change1 = codeChanges.Change(0, codeChanges.Change.insert, 13, " -- this line has been changed")
+        change2 = codeChanges.Change(1, codeChanges.Change.insert, 13, " -- this line has been changed")
+        change3 = codeChanges.Change(5, codeChanges.Change.insert, 13, " -- this line has been changed")
+        change4 = codeChanges.Change(6, codeChanges.Change.insert, 13, " -- this line has been changed")
         self.code.enqueueChange(change1)
         self.code.enqueueChange(change2)
         self.code.enqueueChange(change3)
@@ -79,10 +79,10 @@ class TestCodeChanges(unittest.TestCase):
         self.assertEqual(original2, change2.lineNumber)
     
     def testNewLineShiftsLinesBelow(self):
-        change1 = c.Change(0, c.Change.insert, 13, " -- this line has been changed")
-        change2 = c.Change(1, c.Change.insert, 13, " -- this line has been changed")
-        change3 = c.Change(5, c.Change.insert, 13, " -- this line has been changed")
-        change4 = c.Change(6, c.Change.insert, 13, " -- this line has been changed")
+        change1 = codeChanges.Change(0, codeChanges.Change.insert, 13, " -- this line has been changed")
+        change2 = codeChanges.Change(1, codeChanges.Change.insert, 13, " -- this line has been changed")
+        change3 = codeChanges.Change(5, codeChanges.Change.insert, 13, " -- this line has been changed")
+        change4 = codeChanges.Change(6, codeChanges.Change.insert, 13, " -- this line has been changed")
         self.code.enqueueChange(change1)
         self.code.enqueueChange(change2)
         self.code.enqueueChange(change3)
@@ -96,10 +96,10 @@ class TestCodeChanges(unittest.TestCase):
         self.assertEqual(original4 + 1, change4.lineNumber)
         
     def testNewLineDoesNotShiftLinesAbove(self):
-        change1 = c.Change(0, c.Change.insert, 13, " -- this line has been changed")
-        change2 = c.Change(1, c.Change.insert, 13, " -- this line has been changed")
-        change3 = c.Change(5, c.Change.insert, 13, " -- this line has been changed")
-        change4 = c.Change(6, c.Change.insert, 13, " -- this line has been changed")
+        change1 = codeChanges.Change(0, codeChanges.Change.insert, 13, " -- this line has been changed")
+        change2 = codeChanges.Change(1, codeChanges.Change.insert, 13, " -- this line has been changed")
+        change3 = codeChanges.Change(5, codeChanges.Change.insert, 13, " -- this line has been changed")
+        change4 = codeChanges.Change(6, codeChanges.Change.insert, 13, " -- this line has been changed")
         self.code.enqueueChange(change1)
         self.code.enqueueChange(change2)
         self.code.enqueueChange(change3)
@@ -113,72 +113,72 @@ class TestCodeChanges(unittest.TestCase):
         self.assertEqual(original2, change2.lineNumber)
     
     def testApplyInsertChangesCorrectLine(self):
-        change = c.Change(0, c.Change.insert, 13, " -- this line has been changed")
+        change = codeChanges.Change(0, codeChanges.Change.insert, 13, " -- this line has been changed")
         self.code.applyChange(change)
         self.assertNotEqual("this is line 1", self.code.linesOfCode[0])
         
     def testInsertInsertsAtCorrectIndex1(self):
-        change = c.Change(0, c.Change.insert, 0, "changed -- ")
+        change = codeChanges.Change(0, codeChanges.Change.insert, 0, "changed -- ")
         self.code.applyChange(change)
         self.assertEqual("changed -- this is line 1", self.code.linesOfCode[0])
     
     def testInsertInsertsAtCorrectIndex2(self):
-        change = c.Change(0, c.Change.insert, len(self.code.linesOfCode[0]), " -- changed")
+        change = codeChanges.Change(0, codeChanges.Change.insert, len(self.code.linesOfCode[0]), " -- changed")
         self.code.applyChange(change)
         self.assertEqual("this is line 1 -- changed", self.code.linesOfCode[0])
         
     def testApplyInsertMakesCorrectChanges(self):
-        change = c.Change(0, c.Change.insert, 14, " -- this line has been changed")
+        change = codeChanges.Change(0, codeChanges.Change.insert, 14, " -- this line has been changed")
         self.code.applyChange(change)
         self.assertEqual("this is line 1 -- this line has been changed", self.code.linesOfCode[0])
         
     def testApplyRemoveRemovesFromCorrectLine(self):
-        change = c.Change(0, c.Change.delete, (2, 8), "")
+        change = codeChanges.Change(0, codeChanges.Change.delete, (2, 8), "")
         self.code.applyChange(change)
         self.assertNotEqual("this is line 1", self.code.linesOfCode[0])
         
     def testApplyRemoveCorrectCode1(self):
-        change = c.Change(0, c.Change.delete, (8, 13), "")
+        change = codeChanges.Change(0, codeChanges.Change.delete, (8, 13), "")
         self.code.applyChange(change)
         self.assertEqual("this is 1", self.code.linesOfCode[0])
     
     def testApplyRemoveCorrectCode2(self):
-        change = c.Change(0, c.Change.delete, (0, 5), "")
+        change = codeChanges.Change(0, codeChanges.Change.delete, (0, 5), "")
         self.code.applyChange(change)
         self.assertEqual("is line 1", self.code.linesOfCode[0])
         
     def testApplyChangeDoesNotChangeOtherLines(self):
-        change = c.Change(0, c.Change.insert, 13, " -- this line has been changed")
+        change = codeChanges.Change(0, codeChanges.Change.insert, 13, " -- this line has been changed")
         self.code.applyChange(change)
         for i in range(1, 7):
             self.assertEqual("this is line " + str(i+1), self.code.linesOfCode[i])
             
     def testNewLineInTheMiddleOfALineSplitsTheLine(self):
-        change = c.Change(4, c.Change.newLine, 7, "")
+        change = codeChanges.Change(4, codeChanges.Change.newLine, 7, "")
         self.code.applyChange(change)
         self.assertEqual("this is", self.code.linesOfCode[4])
         self.assertEqual(" line 5", self.code.linesOfCode[5])
 
     def testNewLineSplitMaintainsCode(self):
-        change = c.Change(4, c.Change.newLine, 7, "")
+        change = codeChanges.Change(4, codeChanges.Change.newLine, 7, "")
         originalLine = self.code.linesOfCode[4]
         self.code.applyChange(change)
         self.assertEqual(originalLine, self.code.linesOfCode[4] + self.code.linesOfCode[5])
 
     def testDeleteLineMergesLines(self):
-        change = c.Change(4, c.Change.deleteLine, 0, "")
+        change = codeChanges.Change(4, codeChanges.Change.deleteLine, 0, "")
         self.code.applyChange(change)
         self.assertEqual("this is line 5this is line 6", self.code.linesOfCode[4])
         
     def testNewLineIncreasesLengthOfCode(self):
         initialLength = len(self.code.linesOfCode)
-        change = c.Change(4, c.Change.newLine)
+        change = codeChanges.Change(4, codeChanges.Change.newLine)
         self.code.applyChange(change)
         self.assertEqual(initialLength + 1, len(self.code.linesOfCode))
         
     def testRemoveLineDecreasesLengthOfCode(self):
         initialLength = len(self.code.linesOfCode)
-        change = c.Change(4, c.Change.deleteLine)
+        change = codeChanges.Change(4, codeChanges.Change.deleteLine)
         self.code.applyChange(change)
         self.assertEqual(initialLength - 1, len(self.code.linesOfCode))
         
